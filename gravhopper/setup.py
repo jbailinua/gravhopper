7f20621cbb1f6f6b946edfa5a9c572fb388b0b82
Jeremy Bailin <jbailin@ua.edu>
2021-10-25 17:10:59 -0500

    Edited and moved setup.py to get ready for pip packaging.

--- gravhopper/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-import numpy.distutils.misc_util
-
-setup(
-        ext_modules=[Extension("_jbgrav", ["_jbgrav.c"])],
-        include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
-        )
-
-
---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup, Extension
+import numpy.distutils.misc_util
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+        name="gravhopper",
+        version="1.0.0",
+        author="Jeremy Bailin",
+        author_email="jbailin@ua.edu",
+        description="Simple N-body code for Python",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/jbailinua/gravhopper",
+        classifiers=[ \
+            "Programming Language :: Python :: 3", \
+            "License :: OSI Approved :: BSD License", \
+            "Operating System :: OS Independent",
+        ],
+        package_dir={"":"."},
+        packages=setuptools.find_packages(where="."),
+        python_requires=">=3.6",
+        
+        ext_modules=[Extension("_jbgrav", ["_jbgrav.c"])],
+        include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
+        )
+
+
