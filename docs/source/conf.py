3d1ec622cbbbe1b46d3b7da94e507daa27333c79
jbailinua <jbailin@ua.edu>
2022-12-30 16:46:48 -0600

    Updated docs conf.py to not import _jbgrav

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,3 +56,6 @@ html_theme = 'sphinxdoc'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# autodoc shouldn't try to import the C extension
+autodoc_mock_imports = ["gravhopper._jbgrav"]
